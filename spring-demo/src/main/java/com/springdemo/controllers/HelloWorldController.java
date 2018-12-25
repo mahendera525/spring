6efa5f3f2b4916b31be07cfc54c37139cf2ba81e
mahendera525 <mahenderasingh525@gmail.com>
2018-12-25 10:56:08 +0300

    Adding the new files

---
+++ spring-demo/src/main/java/com/springdemo/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.springdemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String sayHello()
+	{
+		return "index";
+	}
+
+}
