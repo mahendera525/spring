57e840e5f8e918fc9c76867b5cac79d9945f2e5d
mahendera525 <mahenderasingh525@gmail.com>
2019-01-01 10:11:43 +0300

    Added placeholder

---
+++ spring-demo/src/main/java/com/springdemo/web/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.springdemo.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String home()
+	{
+		return "index";
+	}
+}
